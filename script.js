8053d14650beca24c1081d9d748e184ba69db25b
ColasssNotMe <81697922+ColasssNotMe@users.noreply.github.com>
2024-11-12 14:27:56 +0800

    Add HTML structure for multiple pages, implement dark mode functionality, and enhance CSS styles for improved layout and responsiveness

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
 const darkButton = document.getElementById("dark-mode-button");
-console.log(darkButton);
-const switchTheme = (prev) => {
-  !prev;
-};
+const themeIcon = document.getElementById("themeIcon");
+const html = document.documentElement;
+
+var deviceTheme;
+var theme;
+initTheme();
+console.log(deviceTheme);
+
+function initTheme() {
+  if (!localStorage.getItem(theme)) {
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      deviceTheme = "dark";
+      html.setAttribute("data-theme", "dark");
+    } else {
+      deviceTheme = "light";
+      html.setAttribute("data-theme", "light");
+    }
+  }
+}
+
+function toggleMenu(x) {
+  x.classList.toggle("change");
+}
+
+function switchTheme() {
+  html.getAttribute("data-theme") == "light"
+    ? (html.setAttribute("data-theme", "dark"),
+      (themeIcon.src = "res/img/sun.png"),
+      console.log(html.getAttribute("data-themes")))
+    : (html.setAttribute("data-theme", "light"),
+      (themeIcon.src = "res/img/moon.png"),
+      console.log(html.getAttribute("data-themes")));
+}
