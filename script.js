a5b826494f2836e70a6f6433a5452006a8233f5e
ColasssNotMe <81697922+ColasssNotMe@users.noreply.github.com>
2024-11-15 11:45:00 +0800

    Add new HTML pages for terms, privacy, select form, and teacher portal; refactor select subject page and update theme switching logic in script.js

--- script.js
+++ script.js
@@ -1,23 +1,20 @@
+//TODO: update dark mode reference
+
 const darkButton = document.getElementById("dark-mode-button");
 const themeIcon = document.getElementById("themeIcon");
 const html = document.documentElement;
+const body = document.body;
 
 var deviceTheme;
 var theme;
 initTheme();
-console.log(deviceTheme);
 
 function initTheme() {
-  if (!localStorage.getItem(theme)) {
-    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-      deviceTheme = "dark";
-      html.setAttribute("data-theme", "dark");
-      themeIcon.src = "res/img/sun.png";
-    } else {
-      deviceTheme = "light";
-      html.setAttribute("data-theme", "light");
-      themeIcon.src = "res/img/moon.png";
-    }
+  const savedTheme = localStorage.getItem("theme");
+  themeIcon.src =
+    savedTheme == "dark" ? "/res/img/sun.png" : "/res/img/moon.png";
+  if (savedTheme === "dark") {
+    body.classList.add("dark-mode");
   }
 }
 
@@ -26,9 +23,13 @@ function toggleMenu(x) {
 }
 
 function switchTheme() {
-  let currentTheme = html.getAttribute("data-theme");
-  let newTheme = currentTheme === "light" ? "dark" : "light";
-  html.setAttribute("data-theme", newTheme);
-  themeIcon.src = newTheme === "dark" ? "res/img/sun.png" : "res/img/moon.png";
-  localStorage.setItem("theme", newTheme);
+  body.classList.toggle("dark-mode");
+  const isDarkMode = body.classList.contains("dark-mode");
+  theme = isDarkMode ? "dark" : "light";
+  localStorage.setItem("theme", isDarkMode ? "dark" : "light");
+  themeIcon.src = theme == "dark" ? "/res/img/sun.png" : "/res/img/moon.png";
+
+  console.log("Theme switched to:", isDarkMode ? "dark" : "light");
 }
+
+// darkButton.addEventListener("click", switchTheme);
