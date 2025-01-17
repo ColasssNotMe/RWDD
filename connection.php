3e4fe7007eb1944dfcaebcf3fcbd02b9098e6d58
ColasssNotMe <81697922+ColasssNotMe@users.noreply.github.com>
2025-01-17 12:11:25 +0800

    update query

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php
-$server = 'localhost';
-$user = 'root';
+$server   = 'localhost';
+$user     = 'root';
 $password = '';
-$database = 'temp'; // insert database name
+$database = 'quizzation'; // insert database name
 $listOfQuestion;
 
 // TODO: change "table" in the query to db table name
@@ -15,11 +15,10 @@ switch ($connection) {
         break;
 }
 
-
 // get random request question
 function getQuestion($connection, $form, $subject, $numQuestion)
 {
-    $query = "SELECT * from table where form =$_GET[$form] AND subject =$_GET[$subject] ORDER BY RAND() LIMIT $_GET[$numQuestion]";
+    $query  = "SELECT * from question where form =$_GET[$form] AND subject =$_GET[$subject] ORDER BY RAND() LIMIT $_GET[$numQuestion]";
     $result = mysqli_query($connection, $query);
     if (mysqli_num_rows($result) > 0) {
     } else {
@@ -36,15 +35,80 @@ function getQuestion($connection, $form, $subject, $numQuestion)
     }
 } */
 
-
 // used for validate the login user + return the logged in user credential
 function validateUserCredential($connection, $username, $password)
 {
-    $query = "SELECT * FROM table where username=$username AND password=$password";
+    $query  = "SELECT * FROM user where user_name=$username AND password=$password";
     $result = mysqli_query($connection, $query);
     if (mysqli_num_rows($result) > 0) {
         echo 'User found';
         $currentLoginUser = mysqli_num_rows($result);
         return $currentLoginUser;
+    } else {
+        echo "<script>alert('User not found')</script>";
+    }
+}
+
+function addRecord($connection, $score, $timeTaken, $userID, $quizID)
+{
+    $query = "INSERT INTO record (score,time_taken,user_id,quiz_id) VALUES ($score,$timeTaken,$userID,$quizID)";
+    if (!mysqli_query($connection, $query)) {
+        echo "Error inserting data";
+    } else {
+        header("Location:result.php");
+    }
+}
+
+function deleteRecord($connection, $recordID)
+{
+    $query = "DELETE FROM record WHERE record_id = $recordID";
+    if (!mysqli_query($connection, $query)) {
+        echo "Error deleting data";
+    } else {
+        header("Location:result.php");
+    }
+}
+
+function addUser($connection, $username, $password, $role)
+{
+    $query = "INSERT INTO user (user_name,password,role) VALUES ($username,$password,$role)";
+    if (!mysqli_query($connection, $query)) {
+        echo "Error adding user";
+    } else {
+        header("Location:index.php");
+    }
+}
+
+function deleteUser($connection, $userID)
+{
+    $query = "DELETE FROM user WHERE user_id = $userID";
+    if (!mysqli_query($connection, $query)) {
+        echo "Error adding user";
+    } else {
+        header("Location:index.php");
+    }
+}
+
+
+function addQuestion($connection, $form, $subject, $picture, $question, $choice, $answer)
+{
+    $query = "INSERT INTO question 
+    (question_form,question_subject,question_picture,question,question_choice,question_answer) 
+    VALUES 
+   ($form, $subject, $picture,$question,$choice,$answer)";
+    if (!mysqli_query($connection, $query)) {
+        echo "Error adding user";
+    } else {
+        header("Location:index.php");
+    }
+}
+
+function deleteQuestion($connection, $questionID)
+{
+    $query = "DELETE FROM question where question_id =$questionID";
+    if (!mysqli_query($connection, $query)) {
+        echo "Error adding user";
+    } else {
+        header("Location:index.php");
     }
 }
