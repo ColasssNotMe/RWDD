ca416be8daae8eaf548138332b9f62bd39855886
ColasssNotMe <81697922+ColasssNotMe@users.noreply.github.com>
2025-01-25 11:29:35 +0800

    update login and register

--- login.php
+++ login.php
@@ -12,8 +12,34 @@
 
 <body>
     <?php require_once 'components/header.php' ?>
+    <!-- From Uiverse.io by mi-series -->
     <div class="form-container">
-        <form action="" method="POST">
+        <div class="form_area">
+            <p class="form_title">SIGN UP</p>
+            <form action="">
+                <div class="form_group">
+                    <label class="form_sub_title" for="name">Name</label>
+                    <input placeholder="Enter your full name" class="form_style" type="text">
+                </div>
+                <div class="form_group">
+                    <label class="form_sub_title" for="email">Email</label>
+                    <input placeholder="Enter your email" id="email" class="form_style" type="email">
+                </div>
+                <div class="form_group">
+                    <label class="form_sub_title" for="password">Password</label>
+                    <input placeholder="Enter your password" id="password" class="form_style" type="password">
+                </div>
+                <div>
+                    <button class="form_btn">SIGN UP</button>
+                    <p>Have an Account?
+                        <a class="form_link" href="">Login Here!</a>
+                    </p>
+                    <br>
+                </div>
+            </form>
+        </div>
+    </div>
+    <!-- <form action="" method="POST">
             <div class="title">
                 <h1>Login</h1>
                 <h3>
@@ -32,8 +58,7 @@
                 <input type="password" name="password" id="password" />
             </div>
             <button class="primary-button" type="submit" name="login">Login</button>
-        </form>
-    </div>
+        </form>-->
     <script src="./script.js"></script>
     <?php require_once 'components/footer.php' ?>
 </body>
--- register.php
+++ register.php
@@ -16,29 +16,30 @@
 <body>
     <?php require_once 'components/header.php' ?>
     <div class="form-container">
-        <form action="" method="POST">
-            <div class="title">
-                <h1>Register</h1>
-                <h3>
-                    Have an account?
-                    <i><u><a href="./login.php">Login Now</a></u></i>
-                </h3>
-            </div>
-
-            <div class="forms-item">
-                <label for="username">Username</label>
-                <br />
-                <input type="text" name="username" id="username" />
-                <br />
-                <label for="">Password</label>
-                <br />
-                <input type="password" name="password" id="password" />
-                <label for="">Re-enter Password</label>
-                <br />
-                <input type="password" name="repassword" id="repassword" />
-            </div>
-            <button type="submit" name="login">Login</button>
-        </form>
+        <div class="form_area">
+            <p class="form_title">SIGN UP</p>
+            <form action="">
+                <div class="form_group">
+                    <label class="form_sub_title" for="name">Name</label>
+                    <input placeholder="Enter your full name" class="form_style" type="text">
+                </div>
+                <div class="form_group">
+                    <label class="form_sub_title" for="email">Email</label>
+                    <input placeholder="Enter your email" id="email" class="form_style" type="email">
+                </div>
+                <div class="form_group">
+                    <label class="form_sub_title" for="password">Password</label>
+                    <input placeholder="Enter your password" id="password" class="form_style" type="password">
+                </div>
+                <div>
+                    <button class="form_btn">SIGN UP</button>
+                    <p>Have an Account?
+                        <a class="form_link" href="">Login Here!</a>
+                    </p>
+                    <br>
+                </div>
+            </form>
+        </div>
     </div>
     <script src="./script.js"></script>
     <?php require_once 'components/footer.php' ?>
