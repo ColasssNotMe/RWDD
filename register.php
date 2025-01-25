f6ae0d3c6248542ef6abe291090496816a5783f5
Alvin Gan <alvingan8805@gmail.com>
2025-01-25 11:00:16 +0800

    register

--- register.php
+++ register.php
@@ -33,11 +33,13 @@
                 <label for="">Password</label>
                 <br />
                 <input type="password" name="password" id="password" />
+                <br />
                 <label for="">Re-enter Password</label>
                 <br />
                 <input type="password" name="repassword" id="repassword" />
+                <br />
             </div>
-            <button type="submit" name="login">Login</button>
+            <button type="submit" name="login">Register</button>
         </form>
     </div>
     <script src="./script.js"></script>
