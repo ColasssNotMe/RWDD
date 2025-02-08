1626f2ede1ac00ab3f140257f048ae1608de9bc6
ColasssNotMe <81697922+ColasssNotMe@users.noreply.github.com>
2025-02-08 21:31:41 +0800

    refactor about and index pages for improved structure and clarity; enhance styling and layout for better user experience

--- index.php
+++ index.php
@@ -38,21 +38,48 @@
     </div>
   </div>
   <div class="fullSize">
-    <div id="how-it-works">
-      <div class="paragraph">
-        <div id="how-it-works-title"><b>
-            <h1>How It Works?</h1>
-          </b></div>
-        <div>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure,
-            totam enim facilis in perferendis deserunt, et quasi dolores animi
-            consectetur possimus, maxime corporis. Ratione vel itaque numquam
-            impedit ex dicta.
-          </p>
+    <section>
+      <h1>What We Offer</h1>
+
+      <div class="all-cards">
+        <div class="card">
+          <h2><b>Comprehensive SPM Quizzes</b></h2>
+          <p>Covering all major subjects like Mathematics, Science, English, History, and more.</p>
+        </div>
+        <div class="card">
+          <h2><b>Interactive Learning</b></h2>
+          <p>Engaging quizzes, practice questions, and mock exams designed to test your knowledge and identify areas for improvement.</p>
+        </div>
+        <div class="card">
+          <h2><b>24/7 Access</b></h2>
+          <p>Learn at your own pace, anytime, anywhere, with access to our platform 24/7.</p>
         </div>
       </div>
-    </div>
+    </section>
+
+    <section>
+      <div id="how-it-works" class="all-cards">
+        <h1>How It Works?</h1>
+        <div id="steps">
+          <div class="detail-steps">
+            <img src="res/img/usr2.png" alt="" srcset="">
+            <h2><b>Step 1: Login / Register</b></h2>
+            <p>Login or Register an account to keep track of your report</p>
+          </div>
+          <div class="detail-steps">
+            <img src="res/img/check.png" alt="" srcset="">
+            <h2><b>Step 2: Pick</b></h2>
+            <p>Pick the form and subject you want to focus on such as Mathematics, Science, English, or History. This will determine the type of questions you'll encounter in the quiz.</p>
+          </div>
+          <div class="detail-steps">
+            <img src="res/img/play-button.png" alt="" srcset="">
+            <h2><b>Step 3: Start the Quiz!</b></h2>
+            <p>Click the "Start Now" button to begin your quiz. Answer the questions carefully and track your progress. Good luck!</p>
+          </div>
+        </div>
+      </div>
+  </div>
+  </section>
   </div>
   <?php require_once 'components/footer.php' ?>
   <script src="script.js"></script>
