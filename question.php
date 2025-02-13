461db930caedd1491396886e6bd6fa8580d90c00
Alvin Gan <alvingan8805@gmail.com>
2025-02-13 12:15:54 +0800

    question

--- question.php
+++ question.php
@@ -12,21 +12,26 @@ require "connection.php";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Question</title>
     <link rel="stylesheet" href="style/style.css" />
+    <link rel="stylesheet" href="style/question.css" />
 </head>
 
 <body>
     <?php include_once './components/header.php' ?>
     <div class='content'>
         <div id='quiz'>
-            <button name='backBtn'>
-                <a href="select-subject.php"><</a>
-            </button>
-            <h2></h2>
+                
+            <a href="select-subject.php">
+                <button name='backBtn'>
+                    <
+                </button>
+            </a>
+            <h2>Tittle</h2>
             <button name='startBtn'
             class='start-now' onclick=startQuiz()>
             Start Now</button>
+            <h2>Description</h2>
+            <p>[Description]</p>
         </div>
-
     </div>
     <script src="script.js"></script>
 </body>
