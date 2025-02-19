0fe582fe8b0d17a06a8faa2291d49502f6344959
ColasssNotMe <81697922+ColasssNotMe@users.noreply.github.com>
2025-02-19 18:13:20 +0800

    feat: add result section with total and email display, and implement save and return buttons

--- result.php
+++ result.php
@@ -1,5 +1,6 @@
 <?php
 include 'session.php';
+include 'connection.php';
 ?>
 
 <!DOCTYPE html>
@@ -18,6 +19,20 @@ include 'session.php';
     <?php require_once 'components/header.php'; ?>
 
     <div class="content">
+        <div class="result-section">
+            <h1>Result</h1>
+            <p><strong>Total:</strong> </p>
+            <p><strong>Email:</strong> </p>
+            <div class="button-div">
+                <button class="secondary-button">
+                    Save as collection
+                </button>
+                <button class="primary-button" id="return-home">
+                    <a href=<?php echo $index ?> id="return-button">Return to home</a>
+                </button>
+            </div>
+        </div>
+
     </div>
 
     <?php require_once 'components/footer.php'; ?>
